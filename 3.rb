379be6a8f430ec132f08eb637782660811c3f193
guletskaya <guletskayaliza@gmail.com>
2021-12-14 08:42:05 -0800

    refactor:add spaces

--- 3.rb
+++ 3.rb
@@ -1,11 +1,11 @@
 require 'prime'
 
 def prime_factor(n)
-  max=1
+  max = 1
   (2..(n-1)).each do |i|
-    max=i if i.prime?&&max<i&&n%i==0  
-    end
-  if max==1 
+    max=i if i.prime? && max < i && n % i == 0  
+  end
+  if max == 1 
     puts "There are no prime factors"
   else
     puts "The  largest prime factor is #{max}" 
