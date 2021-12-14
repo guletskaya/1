4b271759c315395ed015c1030ed875522136a390
guletskaya <guletskayaliza@gmail.com>
2021-12-14 04:50:36 -0800

    refactor:add indentation, change should_do

--- 2_spec.rb
+++ 2_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
 require_relative '2.rb'
+
 describe 'fib' do
-it 'should_do_fibsum' do
-expect(fib(21)).to eq 10
-end
+  it 'should_do_fibsum' do
+    expect(fib(21)).to eq 10
+  end
 end
 
