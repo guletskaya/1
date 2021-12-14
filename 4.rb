8b9a21d4ee6a7e0d0b818ea9b8fd758a1a9e1a7a
guletskaya <guletskayaliza@gmail.com>
2021-12-14 08:45:05 -0800

    refactor:add spaces

--- 4.rb
+++ 4.rb
@@ -1,13 +1,13 @@
 def isPal?(n)
-  n.to_s==n.to_s.reverse
+  n.to_s == n.to_s.reverse
 end
 
 def pal
-  max=1
+  max = 1
   (100..999).each do |i|
     (i..999).each do |j|
-      if isPal?(i*j) && i*j>max
-      max=i*j
+      if isPal?(i * j) && i * j > max
+      max = i * j
       end
     end
   end
