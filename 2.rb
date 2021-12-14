9d430a325ec2a7bc1088c5425b2f99d563b8c67b
guletskaya <guletskayaliza@gmail.com>
2021-12-14 04:49:33 -0800

    refactor:add indentation,remove extra return

--- 2.rb
+++ 2.rb
@@ -2,15 +2,15 @@ def fib(n)
   a=0
   b=1
   sum=0
-	while b<n
-	  a+=b
-	  b+=a 
-	  if a%2==0  
-	   sum+=a
-	  elsif  b%2==0
-	   sum+=b
-	  end
-	end
+  while b<n
+    a+=b
+    b+=a 
+    if a%2==0  
+      sum+=a
+    elsif  b%2==0
+      sum+=b
+    end
+  end
 sum
 end
 puts fib(4000000)
