a953ead09ab04c7d10b8735a5a208bb539a6868d
guletskaya <guletskayaliza@gmail.com>
2021-12-14 06:23:10 -0800

    refactor:add indentation, remove extra return, remove extra then, use .each over for

--- 9.rb
+++ 9.rb
@@ -1,21 +1,21 @@
 def pythagor
-product=1
-a=1
-b=1
-c=3
-for x in 1..1000 do
-for y in x..1000 do
-z=1000-x-y
-if ((x*x)+(y*y)==(z*z)) then
-a=x
-b=y
-c=z
-  product = a*b*c
-  break
-end
-end
-end
+  product=1
+  a=1
+  b=1
+  c=3
+  (1..1000).each do |x|
+    (x..1000).each do |y|
+      z=1000-x-y
+      if ((x*x)+(y*y)==(z*z)) 
+        a=x
+        b=y
+        c=z
+        product = a*b*c
+        break
+      end
+    end
+  end
 puts "The triplet is: a=#{a}, b=#{b},c=#{c}. The product abc is: #{product}"
-return product
+product
 end
 pythagor
