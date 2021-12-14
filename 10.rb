61e41abbf836911206efd12e9a7b481d7fb7ee40
guletskaya <guletskayaliza@gmail.com>
2021-12-14 08:53:48 -0800

    refactor:add spaces

--- 10.rb
+++ 10.rb
@@ -1,8 +1,8 @@
 require 'prime'
 
 def sum(n)
-  sum=0
-  (2..n-1).each { |k| sum+=k if k.prime? }
+  sum = 0
+  (2..n-1).each { |k| sum += k if k.prime? }
   sum
 end
 puts ("The sum of all the primes below 2000000 is #{sum(2000000)}")
