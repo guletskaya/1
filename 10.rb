42f2834950ebb41186661a8295cc8dd8803e138f
guletskaya <guletskayaliza@gmail.com>
2021-12-14 06:31:32 -0800

    refactor:add indentation, remove extra return, remove prime function, use {..} over do..end

--- 10.rb
+++ 10.rb
@@ -1,17 +1,9 @@
 require 'prime'
 
-def prime?(num)
-    num.prime?
-
-end
 def sum(n)
   sum=0
-  (2..n-1).each do |k|
-    if prime?(k)
-      sum+=k
-    end
-  end
-return sum
-  end
+  (2..n-1).each { |k| sum+=k if k.prime? }
+  sum
+end
 puts ("The sum of all the primes below 2000000 is #{sum(2000000)}")
 
--- 10_spec.rb
+++ 10_spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
 require_relative '10.rb'
+
 describe 'sum' do
-it 'should_do_sum' do
-expect(sum(10)).to eq 17
-end
+  it 'should_do_sum' do
+    expect(sum(10)).to eq 17
+  end
 end
 
