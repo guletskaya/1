ba9c871450d01820b89140249b30dbb54a91fce1
guletskaya <guletskayaliza@gmail.com>
2021-12-14 04:42:35 -0800

    refactor:add indentation,remove extra return, use .each over for, change the language

--- 3.rb
+++ 3.rb
@@ -1,21 +1,15 @@
 require 'prime'
 
-def prime?(num)
-    num.prime?
-end
-
 def prime_factor(n)
-max=1
-for i in 2..(n-1) do
-if prime?(i)&&max<i&&n%i==0
-	max=i  
-	end
-end
-if max==1 
-  puts "Простых делителей нет"
-else
-puts "Максимальный простой делитель #{max}" 
-return max
-end
+  max=1
+  (2..(n-1)).each do |i|
+    max=i if i.prime?&&max<i&&n%i==0  
+    end
+  if max==1 
+    puts "There are no prime factors"
+  else
+    puts "The  largest prime factor is #{max}" 
+  end
+  max
 end
 prime_factor(600851475143)
