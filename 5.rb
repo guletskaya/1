1ad605efa5da12b05648605159fd088167f02de9
guletskaya <guletskayaliza@gmail.com>
2021-12-14 08:15:10 -0800

    refactor:add spaces, move constants outside the function

--- 5.rb
+++ 5.rb
@@ -1,12 +1,12 @@
+FIRST = 2000
+LAST = 243765432
 def small(a,b)
-  FIRST=2000
-  LAST=243765432
-  x=0
+  x = 0
   (FIRST..LAST).each do |i|
-    count=0
-    (a..b).each { |k| count+=1 if i % k==0 }
-    if count==b
-      x=i
+    count = 0
+    (a..b).each { |k| count += 1 if i % k == 0 }
+    if count == b
+      x = i
       break
     end
   end
